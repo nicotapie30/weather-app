67da9397a67363ef68206cdadb345701481ceaa9
Nicolás Tapié <nicoatapie@gmail.com>
2024-12-07 19:00:14 -0300

    add: 03 Obteniendo las URL, APIKEY y creando archivos necesarios para las peticiones de las localidades y su respectivo clima

---
+++ src/services/cities.js
@@ -0,0 +1,1 @@
+const BASE_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=2&appid=03805d208b5c66bf962734ff65caf628`;
---
+++ src/services/weather.js
@@ -0,0 +1,1 @@
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=03805d208b5c66bf962734ff65caf628`;
