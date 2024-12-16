deed37fb210ab45fd7ca39c283d8c7b3ba55b5df
Nicolás Tapié <nicoatapie@gmail.com>
2024-12-16 17:41:33 -0300

    fix: 020 Corrigiendo links de páginas donde se obtuvo la informacion

--- src/components/Footer/Information.jsx
+++ src/components/Footer/Information.jsx
@@ -3,13 +3,25 @@ export const Information = () => {
 		<div className='lg:pl-40 xl:pl-60'>
 			<h3 className='mb-3 text-xl font-bold'>Información</h3>
 			<div className='flex flex-col gap-1 font-medium'>
-				<a className='hover:underline' href='https://react-leaflet.js.org/'>
+				<a
+					className='hover:underline'
+					href='https://react-leaflet.js.org/'
+					target='blank'
+				>
 					<p>React Leaflet</p>
 				</a>
-				<a className='hover:underline' href='https://openweathermap.org/'>
+				<a
+					className='hover:underline'
+					href='https://openweathermap.org/'
+					target='blank'
+				>
 					<p>Open Weather clima</p>
 				</a>
-				<a className='hover:underline' href='https://openweathermap.org/'>
+				<a
+					className='hover:underline'
+					href='https://openweathermap.org/'
+					target='blank'
+				>
 					<p>Open Weather ciudades</p>
 				</a>
 			</div>
