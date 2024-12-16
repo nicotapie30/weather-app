fb7560d4e8cd83c0e90c2f0b52d11e7939c396bc
Nicolás Tapié <nicoatapie@gmail.com>
2024-12-16 20:40:58 -0300

    fix: 023 Corrigiendo errores de RWD para movil

--- src/components/Main/WeatherMain.jsx
+++ src/components/Main/WeatherMain.jsx
@@ -7,11 +7,11 @@ export const WeatherMain = () => {
 
 	return (
 		<>
-			<main className='w-full h-screen lg:px-40 xl:px-80 flex flex-col place-items-center'>
-				<h1 className='mb-6 text-5xl font-bold text-slate-50 max-[500px]:text-4xl'>
+			<main className='w-full h-screen lg:px-40 xl:px-80 flex flex-col place-items-center overflow-auto max-[500px]:mb-10'>
+				<h1 className='mb-6 text-5xl font-bold text-slate-50 max-[500px]:text-4xl max-[500px]:mb-4'>
 					Consulte el clima
 				</h1>
-				<p className='mb-4 text-center text-lg text-stone-200 font-medium'>
+				<p className='mb-4 text-center text-lg text-stone-200 font-medium max-[500px]:text-xs'>
 					Cuanto mas detallado sea, mayor presición tendrá la respuesta
 				</p>
 				<div className='w-4/5 h-auto p-4 max-[500px]:p-2 max-[500px]:w-11/12 bg-white/20 backdrop-blur-3xl text-zinc-50 shadow-md flex flex-col place-items-center gap-4 rounded-md'>
